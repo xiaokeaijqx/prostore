cec0676c70c6081f525bf77e1efe1ad22d7cb19f
kiwa <1430266059@qq.com>
2025-02-05 14:49:29 +0800

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,14 @@ export default function Home() {
           height={38}
           priority
         />
+        <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+        />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
