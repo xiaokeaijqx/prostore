628567a231d057b7ddb1f10c40129792bfa05566
kiwa <1430266059@qq.com>
2025-02-14 14:39:10 +0800

    init

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import sampleData from "@/db/sample-data"
+import sampleData from "@/public/db/sample-data"
 import ProductList from "@/components/shared/product/product-list";
 
 const delay = (ms: number | undefined) => new Promise((resolve) => setTimeout(resolve, ms));
